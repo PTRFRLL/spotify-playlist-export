aa359b8fc6c0014363a09dd8e470c4b7ee2c2a4a
Peter Fiorella <github@fiorella.me>
2024-04-01 17:29:22 -0600

    Remove moment and underscore. Refactor fetching logic. Add delay to avoid rate limit

--- js/app.js
+++ js/app.js
@@ -2,166 +2,215 @@
  * App logic that interacts with Spotify Web API
  * Thanks to https://github.com/plamere/MySavedTracks for example code
  */
-(function() {
-	var playlistExport = {
-		accessToken: null,
-		playlistsObjects: [],
-		tempPlay: null,
-		tracksArray: [],
-		init: function() {
-			this.cacheDom();
-			this.bindEvents();
-			this.pageLoad();
-		},
-		cacheDom: function() {
-			this.$authorize = $('#authorize');
-			this.$message = $('#message');
-			this.$start = $('#start');
-			this.$dir = $('#directions');
-		},
-		bindEvents: function() {
-			this.$authorize.on('click', this.authorize.bind(this));
-			this.$start.on('click', this.export.bind(this));
-		},
-		export: function() {
-			this.message('Exporting your playlists to JSON...');
-			this.$start.hide();
-			playlistExport.printPlaylists(playlistExport.tempPlay);
-		},
-		pageLoad: function() {
-			var args = this.parseArgs();
-			if ('access_token' in args) {
-				accessToken = args['access_token'];
-				playlistExport.$authorize.hide();
-				playlistExport.$dir.hide();
-				this.fetchCurrentUserProfile(function(user) {
-					if (user) {
-						playlistExport.$start.show();
-						playlistExport.fetchPlaylists(function(data) {
-							if (data) {
-								playlistExport.tempPlay = data;
-								playlistExport.message('Found ' + data.total + ' playlists for ' + user.id);
-							}
-						});
-					} else {
-						console.error("Trouble getting the user profile");
-					}
-				});
-			} else {
-				$('#authorize').show();
+(function () {
+  const playlistExport = {
+    accessToken: null,
+    playlists: [],
+    totalPlaylists: 0,
+    nextPageUrl: null,
+    delayInMs: 500,
+    init: function () {
+      this.cacheDom();
+      this.bindEvents();
+      this.pageLoad();
+    },
+    cacheDom: function () {
+      this.$authorize = document.getElementById("authorize");
+      this.$message = document.getElementById("message");
+      this.$start = document.getElementById("start");
+      this.$dir = document.getElementById("directions");
+      this.$progress = document.getElementById("progress");
+    },
+    bindEvents: function () {
+      this.$authorize.addEventListener("click", this.authorize.bind(this));
+      this.$start.addEventListener("click", this.export.bind(this));
+    },
+    export: function () {
+      const estimateTime = Math.ceil(
+        (this.totalPlaylists * this.delayInMs) / 1000 / 60
+      );
+      this.message(
+        `Exporting your playlists to JSON... (eta ${estimateTime} minutes)`
+      );
+      this.$start.style.display = "none";
+      playlistExport.getPlaylists();
+    },
+    pageLoad: async function () {
+      const args = this.parseArgs();
+      if ("access_token" in args) {
+        accessToken = args["access_token"];
+        playlistExport.$authorize.style.display = "none";
+        playlistExport.$dir.style.display = "none";
+        const user = await this.fetchCurrentUserProfile();
+        if (user) {
+          playlistExport.$start.style.display = "inline-block";
+          const playlists = await playlistExport.fetchPlaylists();
+          if (playlists) {
+            playlistExport.playlists = this.formatPlaylists(playlists.items);
+            this.nextPageUrl = playlists.next;
+            console.log(this.playlists);
+            this.totalPlaylists = playlists.total;
+            playlistExport.message(
+              "Found " + playlists.total + " playlists for " + user.id
+            );
+          }
+        } else {
+          console.error("Trouble getting the user profile");
+        }
+      } else {
+        this.$authorize.style.display = "inline-block";
+      }
+    },
+    formatPlaylists: function (playlists) {
+      return playlists.map((playlist) => {
+        return {
+          name: playlist.name,
+          public: playlist.public,
+          collaborative: playlist.collaborative,
+          tracks: playlist.tracks.href,
+        };
+      });
+    },
+    message: function (text) {
+      this.$message.textContent = text;
+    },
+    authorize: function () {
+      const client_id = "75f6c14b0b174008bcaa9171ae7526f5";
+      // const redirect_uri =
+      //   "https://dev.peterfiorella.com/spotifyexport/public/";
+      const redirect_uri = "http://localhost:3000";
+      const scopes = "user-library-read playlist-read-private";
+      const url =
+        "https://accounts.spotify.com/authorize?client_id=" +
+        client_id +
+        "&response_type=token" +
+        "&scope=" +
+        encodeURIComponent(scopes) +
+        "&redirect_uri=" +
+        encodeURIComponent(redirect_uri);
+      document.location = url;
+    },
+    parseArgs: function () {
+      const hash = location.hash.replace(/#/g, "");
+      const all = hash.split("&");
+      const args = {};
+      all.forEach((keyvalue) => {
+        const kv = keyvalue.split("=");
+        const key = kv[0];
+        const val = kv[1];
+        args[key] = val;
+      });
+      return args;
+    },
+    fetchPlaylists: function () {
+      const url = "https://api.spotify.com/v1/me/playlists";
+      return this.callSpotify(url);
+    },
+    fetchCurrentUserProfile: function () {
+      const url = "https://api.spotify.com/v1/me";
+      return this.callSpotify(url);
+    },
+    callSpotify: async function (url) {
+      const res = await fetch(url, {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      });
+      if (!res.ok) {
+        if (res.status === 429) {
+          // throw new Error("Rate limit exceeded");
+          this.message("Rate limit exceeded. Please wait and try again later.");
+        }
+      }
+      return res.json();
+    },
+    delay: function (ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+    setProgress: function (value) {
+      const percent = (value / this.totalPlaylists) * 100;
+      this.$progress.value = percent;
+    },
+    fetchPaginatedData: async function (url, data = []) {
+      try {
+        const response = await this.callSpotify(url);
+        data = data.concat(response.items);
 
-			}
-		},
-		message: function(text) {
-			this.$message.text(text);
-		},
-		authorize: function() {
-			var client_id = '75f6c14b0b174008bcaa9171ae7526f5';
-			var redirect_uri = 'https://dev.peterfiorella.com/spotifyexport/public/';
-			var scopes = 'user-library-read playlist-read-private';
-			var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-				'&response_type=token' +
-				'&scope=' + encodeURIComponent(scopes) +
-				'&redirect_uri=' + encodeURIComponent(redirect_uri);
-			document.location = url;
-		},
-		parseArgs: function() {
-			var hash = location.hash.replace(/#/g, '');
-			var all = hash.split('&');
-			var args = {};
-			_.each(all, function(keyvalue) {
-				var kv = keyvalue.split('=');
-				var key = kv[0];
-				var val = kv[1];
-				args[key] = val;
-			});
-			return args;
-		},
-		fetchPlaylists: function(callback) {
-			var url = 'https://api.spotify.com/v1/me/playlists';
-			this.callSpotify(url, {}, callback);
-		},
-		fetchCurrentUserProfile: function(callback) {
-			var url = 'https://api.spotify.com/v1/me';
-			this.callSpotify(url, null, callback);
-		},
-		callSpotify: function(url, data, callback) {
-			$.ajax(url, {
-				dataType: 'json',
-				data: data,
-				async: false,
-				headers: {
-					'Authorization': 'Bearer ' + accessToken
-				},
-				success: function(r) {
-					callback(r);
-				},
-				error: function(r) {
-					callback(null);
-				}
-			});
-		},
-		printPlaylists: function(playlists) {
-			_.each(playlists.items, function(item) {
-				var name = item.name;
-				var songs = item.tracks.total;
-				var _play = {
-					name: name,
-					public: item.public
-				};
-				var url = item.href + '/tracks';
-				//load tracks array
-				playlistExport.getTracks(url);
-				//assign tracks to playlist obj
-				_play.tracks = playlistExport.tracksArray;
-				//clear tracks array
-				playlistExport.tracksArray = [];
-				playlistExport.playlistsObjects.push(_play);
-			});
+        if (response.next) {
+          return await this.fetchPaginatedData(response.next, data);
+        } else {
+          return data;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    getPlaylists: async function () {
+      const playlistsFromAPI = await this.fetchPaginatedData(this.nextPageUrl);
 
-			if (playlists.next) {
-				this.callSpotify(playlists.next, {}, function(playlists) {
-					playlistExport.printPlaylists(playlists);
-				});
-			} else {
-				this.message('Done!');
-				this.exportJSON(this.playlistsObjects);
-			}
-		},
-		getTracks: function(url) {
-			playlistExport.callSpotify(url, {}, function(tracks) {
-				_.each(tracks.items, function(trk) {
-					var artist;
-					if (trk.track.artists.length > 1) {
-						artist = [];
-						_.each(trk.track.artists, function(_art) {
-							artist.push(_art.name);
-						});
-					} else {
-						artist = trk.track.artists[0].name;
-					}
-					var track = {
-						name: trk.track.name,
-						artist: artist,
-						album: trk.track.album.name,
-						uri: trk.track.uri
-					};
-					playlistExport.tracksArray.push(track);
-				}); //end each
-				if (tracks.next) {
-					playlistExport.getTracks(tracks.next);
-				}
-			}); //end spotify call
-		},
-		exportJSON: function(playlists) {
-			var jsoncontent = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(playlists));
-			var dlAnchorElem = document.getElementById('download');
-			dlAnchorElem.setAttribute("href", jsoncontent);
-			var date = moment().format('YYYY-MM-DD');
-			var filename = "spotify-playlists-" + date + ".json";
-			dlAnchorElem.setAttribute("download", filename);
-			dlAnchorElem.click();
-		}
-	};
-	playlistExport.init();
-})()
+      const playlists = this.formatPlaylists(playlistsFromAPI);
+
+      this.playlists = this.playlists.concat(playlists);
+      console.info(`Finished fetching ${this.playlists.length} playlists`);
+      console.log(this.playlists);
+      this.$progress.style.display = "block";
+      console.info(`Fetching playlist tracks`);
+      await this.getTracks();
+      this.message("Done!");
+      this.exportJSON(this.playlists);
+    },
+    formatTracks: function (tracks) {
+      return tracks.map(({ track }) => {
+        let artist = track.artists.map((artist) => artist.name);
+        if (artist.length === 1) {
+          artist = artist[0];
+        }
+        return {
+          name: track.name,
+          artist: artist,
+          album: track.album.name,
+          uri: track.uri,
+        };
+      });
+    },
+    getTracks: async function () {
+      const playlistsWithTracks = [];
+      for (let playlist of this.playlists) {
+        // Fetch tracks for the current playlist
+        const tracks = await this.fetchPaginatedData(playlist.tracks);
+        playlist.tracks = this.formatTracks(tracks);
+        playlistsWithTracks.push(playlist);
+        this.setProgress(playlistsWithTracks.length);
+        // Delay the next request to avoid hitting the API rate limit
+        await this.delay(this.delayInMs); // Adjust delay time as required by the rate limit
+      }
+
+      this.playlists = playlistsWithTracks;
+    },
+    exportJSON: function (playlists) {
+      const jsoncontent =
+        "data:text/json;charset=utf-8," +
+        encodeURIComponent(JSON.stringify(playlists));
+      const dlAnchorElem = document.getElementById("download");
+      dlAnchorElem.setAttribute("href", jsoncontent);
+      const date = this.formatDate();
+      const filename = "spotify-playlists-" + date + ".json";
+      dlAnchorElem.setAttribute("download", filename);
+      dlAnchorElem.click();
+    },
+    formatDate: function () {
+      const date = new Date();
+
+      let year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      let day = date.getDate();
+
+      // Ensure month and day are two digits
+      month = month < 10 ? "0" + month : month;
+      day = day < 10 ? "0" + day : day;
+
+      return `${year}-${month}-${day}`;
+    },
+  };
+  playlistExport.init();
+})();
